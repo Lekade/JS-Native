bf78e58796e30330f9e60bb4c5dde4fc5e3f045a
Денис Касперович <kasperovich.deonis@gmail.com>
2024-08-19 13:26:39 +0300

    lexical environment

--- Tasks from Chat GPT/lexical environment.js
+++ Tasks from Chat GPT/lexical environment.js
@@ -167,3 +167,114 @@
 // console.log(cachedSquare(5)); // 25
 // console.log(cachedSquare(5)); // 25 (из кеша)
 // console.log(cachedSquare(6)); // 36
+
+
+//Задача 1: Функция-фабрика с инкапсуляцией данных
+// Напишите функцию createCounter на основе замыкания, которая создает счетчик с возможностью увеличивать, уменьшать и сбрасывать значение. Интерфейс работы с счетчиком должен включать методы increment, decrement и reset.
+//
+// const createCounter = () => {
+//     let counter = 0;
+//     return {
+//         increment: () => ++counter,
+//         decrement: () => --counter,
+//         reset: () => counter = 0
+//     };
+// };
+//
+// const counter = createCounter();
+//
+// console.log(counter.increment()); // 1
+// console.log(counter.increment()); // 2
+// console.log(counter.decrement()); // 1
+// counter.reset();
+// console.log(counter.increment()); // 1
+
+
+// Задача 2: Фабрика функций с доступом к контексту
+// Напишите функцию createPerson, которая принимает имя и возраст и возвращает объект с методами getName, getAge и growOlder. Метод growOlder должен увеличивать возраст на 1 год.
+//
+
+// const createPerson = (name, age) => {
+//     return {
+//         getName: () => name,
+//         getAge: () => age,
+//         growOlder: () => ++age
+//
+//     }
+// }
+//
+//
+// const person = createPerson('Alice', 30);
+//
+// console.log(person.getName()); // 'Alice'
+// console.log(person.getAge()); // 30
+// person.growOlder();
+// console.log(person.getAge()); // 31
+
+
+// Задача 3: Счетчик с раздельным увеличением
+// Напишите функцию createMultipleCounters, которая создает несколько независимых счетчиков, каждый со своим шагом увеличения. Функция должна возвращать массив функций-счетчиков. Каждая функция-счетчик должна увеличивать свое значение на заданный шаг и возвращать его.
+//
+
+// const createMultipleCounters = (steps) => {
+//     const counters = new Array(steps.length).fill(0);
+//
+//     return steps.map((step, index) => () => counters[index] += step);
+// };
+//
+//
+// const [counter1, counter2, counter3] = createMultipleCounters([1, 2, 3]);
+//
+// console.log(counter1()); // 1
+// console.log(counter2()); // 2
+// console.log(counter3()); // 3
+// console.log(counter1()); // 2
+// console.log(counter2()); // 4
+// console.log(counter3()); // 6
+
+
+// Задача 4: Хранилище данных с функцией доступа
+// Напишите функцию createStorage, которая создает хранилище данных. Функция должна возвращать два метода: addItem, который добавляет элемент в хранилище, и getItems, который возвращает все элементы хранилища.
+//
+
+// const createStorage = () => {
+//     const storage = []
+//     return {
+//         addItem: (el) => {
+//             storage.push(el)
+//             return storage.length
+//         },
+//         getItems: () => [...storage]
+//     }
+// }
+//
+// const storage = createStorage();
+//
+// storage.addItem('apple');
+// storage.addItem('banana');
+// console.log(storage.getItems()); // ['apple', 'banana']
+
+
+// Задача 5: Лексическое окружение в цикле
+// Напишите функцию createDelayedLoggers, которая создает массив функций, каждая из которых выводит в консоль индекс своего создания с задержкой. Задержка для каждой функции должна быть пропорциональна её индексу (например, первая функция выводит через 1 секунду, вторая — через 2 секунды и т.д.).
+//
+
+// const createDelayedLoggers = (length) => {
+//     let arr = []
+//     for(let i = 0; i < length; i++) {
+//         arr.push(() => {
+//             setTimeout(()=>{
+//                 console.log(i)
+//             }, (i + 1) * 1000)
+//         })
+//     }
+//     return Object.freeze([...arr]);
+// }
+//
+// const loggers = createDelayedLoggers(5);
+//
+// loggers[0] =  10 // игнорируется
+//
+// loggers[0](); // выведет 0 через 1 секунду
+// loggers[1](); // выведет 1 через 2 секунды
+// loggers[2](); // выведет 2 через 3 секунды
